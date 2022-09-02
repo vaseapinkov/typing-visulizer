e1807b64770041ba3e5ef055cd6a3fa09498e7fa
vaseapinkov <wasea.pinkov@gmail.com>
2022-09-02 09:48:09 +0300

    Fix styles, added new inputs

--- resources/js/Pages/SamsungInputTest.vue
+++ resources/js/Pages/SamsungInputTest.vue
@@ -1,21 +1,35 @@
 <template>
-    <h1>Input 1</h1>
-    <input type="file" accept="image/*,video/*" />
+    <div>
+        <h1>Input 1</h1>
+        <input type="file" accept="image/*,video/*" />
 
-    <h1>Input 2</h1>
-    <input type="file" accept="image/jpg,image/png,video/*" />
+        <h1>Input 2</h1>
+        <input type="file" accept="image/jpg,image/png,video/*" />
 
-    <h1>Input 3</h1>
-    <input type="file" accept="image/jpg,image/png,image/jpeg,video/*" />
+        <h1>Input 3</h1>
+        <input type="file" accept="image/jpg,image/png,image/jpeg,video/*" />
 
-    <h1>Input 4</h1>
-    <input type="file" accept="image/*" />
+        <h1>Input 4</h1>
+        <input type="file" accept="image/*" />
 
-    <h1>Input 5</h1>
-    <input type="file" accept="image/jpg,image/png" />
+        <h1>Input 5</h1>
+        <input type="file" accept="image/jpg,image/png" />
 
-    <h1>Input 6</h1>
-    <input type="file" accept="image/jpg,image/png,image/jpeg" />
+        <h1>Input 6</h1>
+        <input type="file" accept="image/jpg,image/png,image/jpeg" />
+
+        <h1>Input 7</h1>
+        <input type="file" accept="image/*" capture />
+
+        <h1>Input 8</h1>
+        <input type="file" accept="image/*" capture="user" />
+
+        <h1>Input 9</h1>
+        <input type="file" accept="image/*" capture="environment" />
+
+        <h1>Input 10</h1>
+        <input type="file" accept="image/jpg,image/png,video/*" />
+    </div>
 </template>
 
 <script>
@@ -25,7 +39,11 @@ export default {
 </script>
 
 <style scoped>
+div {
+    @apply ml-4;
+}
+
 h1 {
-    @apply h2, mt-4;
+    @apply mt-4 text-xl font-semibold;
 }
 </style>
